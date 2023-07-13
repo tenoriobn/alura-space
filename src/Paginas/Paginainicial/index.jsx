918517d0f9c8cd4176b8b1660fece51648ab01ca
Bruno Tenório <tenoriobn@gmail.com>
2023-07-13 15:10:14 -0300

    Adicionando o banner

--- src/Paginas/Paginainicial/index.jsx
+++ src/Paginas/Paginainicial/index.jsx
@@ -1,12 +1,22 @@
 import Cabecalho from "../../Componentes/Cabecalho";
 import Menu from "../../Componentes/menu";
-
+import styles from './PaginaInicial.module.scss';
+import banner from './banner.png'
 
 export default function Paginainicial() {
     return (
         <>
             <Cabecalho />
-            <Menu />
+
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço" />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
