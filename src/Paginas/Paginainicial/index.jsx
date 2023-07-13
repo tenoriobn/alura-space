628e613ee4ee8b5c731a3afdc62f861c063d033f
Bruno Tenório <tenoriobn@gmail.com>
2023-07-13 15:01:40 -0300

    Criando e estilizando Menu

--- src/Paginas/Paginainicial/index.jsx
+++ src/Paginas/Paginainicial/index.jsx
@@ -1,7 +1,12 @@
 import Cabecalho from "../../Componentes/Cabecalho";
+import Menu from "../../Componentes/menu";
+
 
 export default function Paginainicial() {
     return (
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <Menu />
+        </>
     )
 }
