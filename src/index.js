a779a966746ad6a488a9e6fb63be2b06783d4804
Bruno Tenório <tenoriobn@gmail.com>
2023-07-14 14:39:10 -0300

    Adicionando fonte ao projeto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "../src/assets/fontes/GandhiSans-Regular.otf";
+import "../src/assets/fontes/GandhiSans-Bold.otf";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
